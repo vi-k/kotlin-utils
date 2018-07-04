96b3a41c982335ac4e9d40b7f79befa0e6493dfc
Victor <victor.dunaev@gmail.com>
2018-07-04 17:44:41 +1000

    Добавил в Document() парсинг Plain text

--- src/main/java/ru/vik/utils/document/Document.kt
+++ src/main/java/ru/vik/utils/document/Document.kt
@@ -1,6 +1,19 @@
 package ru.vik.utils.document
 
+import ru.vik.utils.parser.StringParser
+
 open class Document : Section() {
     open fun setText(text: String) {
+        val parser = StringParser(text)
+
+        while (!parser.eof()) {
+            parser.start()
+            while (!parser.eof() && parser.get() != '\n') {
+                parser.next()
+            }
+
+            this.addParagraph(Paragraph(text.substring(parser.start, parser.pos)))
+            parser.next()
+        }
     }
 }
