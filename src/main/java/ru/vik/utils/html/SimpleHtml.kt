4c38e76d24049daeee80cf72626ce269bda3d3ba
Victor <victor.dunaev@gmail.com>
2018-07-06 14:39:04 +1000

    Добавил несколько тегов

--- src/main/java/ru/vik/utils/html/SimpleHtml.kt
+++ src/main/java/ru/vik/utils/html/SimpleHtml.kt
@@ -3,17 +3,22 @@ package ru.vik.utils.html
 open class SimpleHtml : BaseHtml() {
 
     init {
-        addTag("b", TagConfig(type = Tag.Type.CHARACTER))
         addTag("br", TagConfig(type = Tag.Type.BR))
+
         addTag("div", TagConfig(type = Tag.Type.SECTION))
+
+        addTag("p", TagConfig(type = Tag.Type.PARAGRAPH))
         addTag("h1", TagConfig(type = Tag.Type.PARAGRAPH))
         addTag("h2", TagConfig(type = Tag.Type.PARAGRAPH))
         addTag("h3", TagConfig(type = Tag.Type.PARAGRAPH))
+        addTag("blockquote", TagConfig(type = Tag.Type.PARAGRAPH))
+
+        addTag("b", TagConfig(type = Tag.Type.CHARACTER))
         addTag("i", TagConfig(type = Tag.Type.CHARACTER))
-        addTag("p", TagConfig(type = Tag.Type.PARAGRAPH))
         addTag("s", TagConfig(type = Tag.Type.CHARACTER))
+        addTag("strike", getTagConfig("s")!!)
         addTag("span", TagConfig(type = Tag.Type.CHARACTER))
-        addTag("strike", TagConfig(type = Tag.Type.CHARACTER))
+        addTag("font", getTagConfig("span")!!)
         addTag("sub", TagConfig(type = Tag.Type.CHARACTER))
         addTag("sup", TagConfig(type = Tag.Type.CHARACTER))
         addTag("small", TagConfig(type = Tag.Type.CHARACTER))
