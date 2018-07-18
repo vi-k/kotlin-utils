81fcedf90d6d71f3d8b4947769822df7d2c8fffb
Victor <victor.dunaev@gmail.com>
2018-07-18 23:44:01 +1000

    BorderStyle сделал не обязательным. Добавил свойство text. Стили сделал муттабельными

--- src/main/java/ru/vik/utils/document/Section.kt
+++ src/main/java/ru/vik/utils/document/Section.kt
@@ -3,9 +3,18 @@ package ru.vik.utils.document
 open class Section : ParagraphItem {
     var parent: Section? = null
 
-    override val borderStyle = BorderStyle()
-    override val paragraphStyle = ParagraphStyle()
-    override val characterStyle = CharacterStyle()
+    final override var borderStyle = BorderStyle()
+    final override var paragraphStyle = ParagraphStyle()
+    final override var characterStyle = CharacterStyle()
+
+    override val text: CharSequence get() {
+        val out = StringBuilder()
+        for (item in this._paragraphs) {
+            if (out.isNotEmpty()) out.append('\n')
+            out.append(item.text)
+        }
+        return out
+    }
 
     private val _paragraphs = mutableListOf<ParagraphItem>()
     val paragraphs: List<ParagraphItem> get() = this._paragraphs
@@ -42,21 +51,35 @@ open class Section : ParagraphItem {
     }
 
     override fun addSpan(start: Int, end: Int, characterStyle: CharacterStyle,
-        borderStyle: BorderStyle
+        borderStyle: BorderStyle?
     ): Section {
         this._paragraphs[0].addSpan(start, end, characterStyle, borderStyle)
         return this
     }
 
+    override fun addSpan(regex: Regex, count: Int, characterStyle: CharacterStyle,
+        borderStyle: BorderStyle?
+    ): Section {
+        this._paragraphs[0].addSpan(regex, count, characterStyle, borderStyle)
+        return this
+    }
+
+    override fun addSpan(regex: Regex, characterStyle: CharacterStyle,
+        borderStyle: BorderStyle?
+    ): Section {
+        this._paragraphs[0].addSpan(regex, characterStyle, borderStyle)
+        return this
+    }
+
     override fun addWordSpan(numberOfWord: Int, characterStyle: CharacterStyle,
-        borderStyle: BorderStyle
+        borderStyle: BorderStyle?
     ): Section {
         this._paragraphs[0].addWordSpan(numberOfWord, characterStyle, borderStyle)
         return this
     }
 
     override fun addWordSpan(numberOfWord: Int, count: Int, characterStyle: CharacterStyle,
-        borderStyle: BorderStyle
+        borderStyle: BorderStyle?
     ): Section {
         this._paragraphs[0].addWordSpan(numberOfWord, count, characterStyle, borderStyle)
         return this
@@ -65,21 +88,4 @@ open class Section : ParagraphItem {
     override fun findWord(numberOfWord: Int): Int {
         return this._paragraphs[0].findWord(numberOfWord)
     }
-
-//    override fun setMargin(margin: Size?): Section = super.setMargin(margin) as Section
-//    override fun setMargin(topAndBottom: Size?, leftAndRight: Size?): Section = super.setMargin(topAndBottom, leftAndRight) as Section
-//    override fun setMargin(top: Size?, leftAndRight: Size?, bottom: Size?): Section = super.setMargin(top, leftAndRight, bottom) as Section
-//    override fun setMargin(top: Size?, right: Size?, bottom: Size?, left: Size?): Section = super.setMargin(top, right, bottom, left) as Section
-//
-//    override fun setPadding(padding: Size?): Section = super.setPadding(padding) as Section
-//    override fun setPadding(topAndBottom: Size?, leftAndRight: Size?): Section = super.setPadding(topAndBottom, leftAndRight) as Section
-//    override fun setPadding(top: Size?, leftAndRight: Size?, bottom: Size?): Section = super.setPadding(top, leftAndRight, bottom) as Section
-//    override fun setPadding(top: Size?, right: Size?, bottom: Size?, left: Size?): Section = super.setPadding(top, right, bottom, left) as Section
-//
-//    override fun setBorder(border: Border?): Section = super.setBorder(border) as Section
-//    override fun setBorder(topAndBottom: Border?, leftAndRight: Border?): Section = super.setBorder(topAndBottom, leftAndRight) as Section
-//    override fun setBorder(top: Border?, leftAndRight: Border?, bottom: Border?): Section = super.setBorder(top, leftAndRight, bottom) as Section
-//    override fun setBorder(top: Border?, right: Border?, bottom: Border?, left: Border?): Section = super.setBorder(top, right, bottom, left) as Section
-//
-//    override fun setBackgroundColor(color: Int): Section = super.setBackgroundColor(color) as Section
 }
