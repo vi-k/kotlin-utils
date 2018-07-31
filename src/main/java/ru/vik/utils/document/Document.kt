426074726b6ee37ea80b3e144505c6322c3592dd
Victor <victor.dunaev@gmail.com>
2018-08-01 01:07:58 +1000

    Наконец нашёл способ сделать invoke и для наследников

--- src/main/java/ru/vik/utils/document/Document.kt
+++ src/main/java/ru/vik/utils/document/Document.kt
@@ -1,8 +1,11 @@
 package ru.vik.utils.document
 
-open class Document : Section() {
-    operator fun invoke(init: Document.() -> Unit): Document {
-        this.init()
-        return this
-    }
+open class Document : Section()
+
+// Invoke внутри класса позволяет работать только с самим классом - в наследниках её можно
+// применять только по отношению к родительскому классу. Данная же конструкция позволяет
+// и наследникам полноценно использовать invoke.
+operator fun <T : Document> T.invoke(init: T.() -> Unit): T {
+    this.init()
+    return this
 }
